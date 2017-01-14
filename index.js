50b57e9dbdb6b33d808e5de3b04183f5ba1ea476
Vikas Gautam <vikasgautam43@gmail.com>
2017-01-15 01:58:38 +0530

    Some changes

--- index.js
+++ index.js
@@ -4,11 +4,10 @@
       if (doThis.doThis) { onError = doThis.onError; doThis = doThis.doThis} // Mapping arguments
       var Promise = this.constructor
       return this.then(function makeChainedPromise(array) {
-         var resolve = Promise.resolve || Promise.when
          if (!Array.isArray(array)) { context = array.context;  array = array.array } // Mapping arguments
          return array.reduce(function iterator(Promise, item, index) {
             return Promise.then(doThis.bind(null, item, index)).catch(onError && onError.bind(null, item, index))
-         }, resolve(context))
+         }, Promise.resolve(context))
       })
    }
 
