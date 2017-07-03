4f946dac43c95577a3b6583ed850d1a98df7d691
Vikas Gautam <vikasgautam43@gmail.com>
2017-07-03 07:18:00 +0530

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-// The pattern of extending global or third party class has been copied from rxjs/add/operator in angular2
-
-/*
-function thenForEach<R>(doFn: (item?: any, index?: number, context?: any) => Promise<R>|R, context?: any): Promise<R|{value: R, context: any}> {
-   return this.then((arr: Array<any>): Promise<R> => {
-      if (!Array.isArray(arr)) throw "Error: thenForEach must receive an array."
-      return arr.reduce((Promise: Promise<R>, item: any, index: number) => {
-         return Promise.then(doFn.bind(null, item, index, context))
-      }, this).then((value: R) => { return context ? {value, context} : value })
-   })
-}
-*/
-
 function thenForEach<T>(doFn: (item?: T, index?: number, context?: any) => void|Promise<void>, context?: any): Promise<any> {
    return this.then(validate).then(() => context)
    function validate(arrOrIterable: Array<any>|IterableIterator<any>): Promise<void> {
@@ -41,6 +28,7 @@ function iterateIterable<T>(iterable: IterableIterator<T>, i: number, context: a
       .then(() => arguments.callee(iterable, i, context, doFn))
 }
 
+// The pattern of extending global or third party class has been copied from rxjs/add/operator in angular2
 Promise.prototype.thenForEach = thenForEach
 
 // Extending interface to let Typescript compiler help us.
